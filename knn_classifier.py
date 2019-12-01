e731e7346e35b1719a923f72d5e4330ddbe8f302
cwjjsgw <cwjjsgw@gmail.com>
2019-12-01 18:45:48 -0500

    Update knn_classifier.py

--- knn_classifier.py
+++ knn_classifier.py
@@ -1,1 +1,13 @@
 # build classifer
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+#create own knn with our designed distance
+#k is neighbors number
+def myknn(k,x_train,y_train,x_test,y_test,method):
+    knn=KNeighborsClassifier(n_neighbors=k,weights=method)
+    knn.fit(x_train,y_train)
+    y_pred = knn.predict(x_test)
+    print(metrics.accuracy_score(y_test, y_pred))
+    
