52a994f4e207c8a7b74dd561cfd67c7d66fc2eb1
jiesparklin <33667998+jiesparklin@users.noreply.github.com>
2019-12-02 18:25:40 -0500

    Update hott.py

--- hott.py
+++ hott.py
@@ -1,16 +1,14 @@
-from scipy.stats import wasserstein_distance
 ## Quoting from the paper: When computing HOTT, we simply truncate LDA topic proportions at 1/(|T | + 1), 
 ## the value below LDA’s uniform topic proportion prior, and re-normalize. 
-def hott(u,v,u_weight=None,v_weight=None,prior = -999): ### u and v take LDA topic proportions; weights take the NBOW perhaps
+def HOTT(u,v,C,prior = -999): ### u and v take LDA topic proportions; weights take the NBOW perhaps
     if prior == -999:
         prior = 1./(len(u) + 1);
     u_new = u[np.where(u > prior)];
     v_new = v[np.where(v > prior)];
-    #u_weight_new =u_weight[np.where(u>prior)];
-    #v_weight_new = v_weight[np.where(v>prior)];
-    return wasserstein_distance(u_new,v_new)
+    C_new = C[np.where(u > prior)][:,np.where(v > prior)]
+    return ot.emd2(u_new,v_new,C_new)
 
 
 
-def HOFTT(u,v,u_weight,v_weight)
-    return wasserstein_distance(u,v,u_weight, v_weight)
+def HOFTT(u,v,C):
+    return ot.emd2(u,v,C)
