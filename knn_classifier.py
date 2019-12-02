450f32cb4151e3bbadb54ce8ad2b816d6b5eadef
cwjjsgw <cwjjsgw@gmail.com>
2019-12-02 02:43:27 -0500

    Update knn_classifier.py

--- knn_classifier.py
+++ knn_classifier.py
@@ -1,13 +1,40 @@
 # build classifer
 import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
 
-#create own knn with our designed distance
-#k is neighbors number
-def myknn(k,x_train,y_train,x_test,y_test,method):
-    knn=KNeighborsClassifier(n_neighbors=k,weights=method)
-    knn.fit(x_train,y_train)
-    y_pred = knn.predict(x_test)
-    print(metrics.accuracy_score(y_test, y_pred))
+def knnclassify(test,x_train,y_train,k,method):
+    distances = []
     
+    for x in range(len(x_train)):
+        dist = method(x_train[x], y_train[x])
+        distances.append(dist)
+    sortindices=np.argsort(dist)
+    neighbors = []
+    for x in range(k):
+        neighbors.append(x_train[sortindices[x]])  #find k nearest neighbors
+    
+    classvote={}#count the frequency
+    nclass = np.unique(neighbors)
+    for i in range(len(nclass)):
+        temp=neighbors.count(nclass[1])
+        classvote.append(temp)
+        
+    predict=np.argmax(classvote)
+    
+    return nclass[predict]#return the most popular one 
+
+def myknn(k,x_train,y_train,x_test,y_test,method):
+    y_pred=[]
+    for i in range(len(x_test)):
+        ypred=knnclassify(x_test[i],x_train,y_train,k,method)
+        y_pred.append(ypred)
+    
+    count=0
+    for i in range(len(y_test)):
+        if y_pred==y_test:
+            count=count+1
+            
+            
+    test_error=1-count/len(y_test)
+    
+    return test_error
+        
