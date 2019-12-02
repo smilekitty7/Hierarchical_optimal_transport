727af349fff92b300f69e98d9df6dd8a8a27f58d
cwjjsgw <cwjjsgw@gmail.com>
2019-12-02 11:43:56 -0500

    Update knn_classifier.py

--- knn_classifier.py
+++ knn_classifier.py
@@ -1,26 +1,32 @@
 # build classifer
+# -*- coding: utf-8 -*-
+
+import operator
 import numpy as np
+from collections import Counter
+
 
 def knnclassify(test,x_train,y_train,k,method):
     distances = []
     
     for x in range(len(x_train)):
-        dist = method(x_train[x], y_train[x])
+        dist = method(test, x)
         distances.append(dist)
-    sortindices=np.argsort(dist)
+    neighbor_inx=np.argsort(dist)[:k]
     neighbors = []
     for x in range(k):
-        neighbors.append(x_train[sortindices[x]])  #find k nearest neighbors
+        neighbors.append(y_train[neighbor_inx[x]])  
     
-    classvote={}#count the frequency
-    nclass = np.unique(neighbors)
-    for i in range(len(nclass)):
-        temp=neighbors.count(nclass[1])
-        classvote.append(temp)
-        
-    predict=np.argmax(classvote)
-    
-    return nclass[predict]#return the most popular one 
+#    classvote={}
+#    nclass = np.unique(neighbors)
+#    for i in range(len(nclass)):
+#        temp=neighbors.count(nclass[i])
+#        classvote.append(temp)
+#        
+#    predict=np.argmax(classvote)
+#    
+    most_common = Counter(neighbors).most_common(1)
+    return most_common[0][0]
 
 def myknn(k,x_train,y_train,x_test,y_test,method):
     y_pred=[]
@@ -37,4 +43,10 @@ def myknn(k,x_train,y_train,x_test,y_test,method):
     test_error=1-count/len(y_test)
     
     return test_error
+        
+
+
+
+
+
         
