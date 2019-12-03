a09c75b8a2ea8f69cd46ee05aeb86c876d9d6378
jiesparklin <33667998+jiesparklin@users.noreply.github.com>
2019-12-02 20:07:25 -0500

    Update other_distance.py

--- other_distance.py
+++ other_distance.py
@@ -6,30 +6,28 @@
 ### a geometry on the words in V . A corpus D = {d1, d2, . . . d|D|} can be represented using distributions
 ### over V via a normalized BOW.... The WMD between documents d1 and d2 is then WMD(d1, d2) = W1(d1, d2).
 from scipy.stats import wasserstein_distance
-def wmd(u,v,u_weight,v_weight): ### u and v take two probability distributions; weight perhaps take NBOW
-   return wasserstein_distance(u,v,u_weight, v_weight)
-#### WMD-T20: find distances between the top 20 words.
-import numpy as np
-def wmdT20(u,v,u_weight,v_weight): 
+def wmd(u,v): ### u and v take two probability distributions; weight perhaps take NBOW
+   return wasserstein_distance(u,v)
+
+def wmdT20(u,v): 
     idx_u = np.argsort(u)
     idx_v = np.argsort(v)
-    u_t20 = np.zeros(len(indx_u[:20]))
-    uw_t20 = np.zeros(len(indx_u[:20]))
-    v_t20 = np.zeros(len(indx_v[:20]))
-    vw_t20 = np.zeros(len(indx_v[:20]))
+    u_t20 = np.zeros(len(idx_u[:20]))
+    v_t20 = np.zeros(len(idx_v[:20]))
     k = 0
     n = 0
     
     for i in idx_u[:20]:
         u_t20[k] = u[i]
-        uw_t20[k] = u_weight[i]
+
         k+=1
     for ii in idx_v[:20]:
         v_t20[n] = v[ii]
-        vw_t20[n] = v_weight[ii]
+
         n+=1
 
-    return wasserstein_distance(u_t20,v_t20,uw_t20, vw_t20)
+    return wasserstein_distance(u_t20,v_t20)
+
 #### RWMD: code modified from https://github.com/mkusner/wmd/blob/master/compute_rwmd.m
 def RWMD(u,v,u_BOW,v_BOW):
     import math
