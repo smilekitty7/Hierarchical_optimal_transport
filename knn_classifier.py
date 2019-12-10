1a9012cf31fcab41e6b09a135f7695dfb789f00f
jiesparklin <33667998+jiesparklin@users.noreply.github.com>
2019-12-10 17:01:10 -0500

    Update knn_classifier.py

--- knn_classifier.py
+++ knn_classifier.py
@@ -21,25 +21,14 @@ def knnclassify(test,x_train,y_train,D,k,method):
     for x in range(len(neighbor_inx)):
         
         neighbors.append(y_train[neighbor_inx[x]])  
-    
-#    classvote={}
-#    nclass = np.unique(neighbors)
-#    for i in range(len(nclass)):
-#        temp=neighbors.count(nclass[i])
-#        classvote.append(temp)
-#        
-#    predict=np.argmax(classvote)
-#    
+  
     most_common = Counter(neighbors).most_common(1)
     return most_common[0][0]
 
 def myknn(k,x_train,y_train,x_test,y_test,D,method):
     y_pred=[]
     tempcount = 0
-    #x_train = x_train.T
     for i in range(len(x_test)):
-        #print(x_test[i].shape)
-        #wait = input("press enter:")
         ypred_t=knnclassify(x_test[i],x_train,y_train,D,k,method)
         y_pred.append(ypred_t)
         tempcount = tempcount+1
