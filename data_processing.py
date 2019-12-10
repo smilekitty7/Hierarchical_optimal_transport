2d526edf7ac9b1409b6c7c8e659138dffec51aa3
smilekitty7 <mengxue.kang@gmail.com>
2019-12-10 16:00:38 -0500

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -2,68 +2,85 @@ import numpy as np
 import lda
 import ot
 
-from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics.pairwise import euclidean_distances, cosine_similarity
 import scipy.io as sio
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 from nltk.stem.snowball import SnowballStemmer
 
-
+embeddings_path = 'glove.6B.300d.txt'
 # load data from the WMD paper
 # each data file contains the words, the embedding vector for each word, the bow vector for each word,
 
 
 # Reduce vocabulary size by stemming and removing stop words.
-def reduce_vocab(bow_data, vocab, embed_vocab, embed_aggregate='mean'):
-    """Reduce vocabulary size by stemming and removing stop words.
-    """
-    vocab = np.array(vocab)
-    short = np.array([len(w) > 2 for w in vocab])
-    stop_words = set(stopwords.words('english'))
-    stop = np.array([w not in stop_words for w in vocab])
-    reduced_vocab = vocab[np.logical_and(short, stop)]
-    reduced_bow_data = bow_data[:, np.logical_and(short, stop)]
+def stem_vocab(vocab_BOW, vocab, vocab_embed):
+    # do stemming, stop word removal, short word removal, infrequent word removal.
+
+    # 1. stemming
+    vocab_new = []
+    emb = {}
     stemmer = SnowballStemmer("english")
-    stemmed_dict = {}
-    stemmed_idx_mapping = {}
-    stemmed_vocab = []
-    for i, w in enumerate(reduced_vocab):
-        stem_w = stemmer.stem(w)
-        if stem_w in stemmed_vocab:
-            stemmed_dict[stem_w].append(w)
-            stemmed_idx_mapping[stemmed_vocab.index(stem_w)].append(i)
+    idx = {}
+
+    for i, word in enumerate(vocab):
+        word1 = stemmer.stem(word) # stemmed word
+        if word1 in vocab_new:
+            #print('yes')
+            emb[word1].append(vocab_embed[word])
+            idx[word1].append(i)
         else:
-            stemmed_dict[stem_w] = [w]
-            stemmed_vocab.append(stem_w)
-            stemmed_idx_mapping[stemmed_vocab.index(stem_w)] = [i]
+            vocab_new.append(word1)
+            emb[word1] = [vocab_embed[word]]
+            idx[word1] = [i]
 
-    stemmed_bow_data = np.zeros((bow_data.shape[0], len(stemmed_vocab)),
-                                dtype=np.int)
-    for i in range(len(stemmed_vocab)):
-        stemmed_bow_data[:, i] = reduced_bow_data[:, stemmed_idx_mapping[i]].sum(axis=1).flatten()
 
-    word_counts = stemmed_bow_data.sum(axis=0)
-    stemmed_reduced_vocab = np.array(stemmed_vocab)[word_counts > 5].tolist()
-    stemmed_reduced_bow_data = stemmed_bow_data[:, word_counts > 5]
+    vocab_BOW_new = np.zeros((vocab_BOW.shape[0], len(vocab_new)))
+    for i in range(len(vocab_new)):
+        voc = vocab_new[i]
+        vocab_BOW_per_doc = vocab_BOW[:, idx[voc]]
+        #print(vocab_BOW_per_doc.shape)
+        #print(np.sum(vocab_BOW_per_doc, axis=1).flatten())
+        #print('---')
+        vocab_BOW_new[:, i] = np.sum(vocab_BOW_per_doc, axis=1).flatten()
 
-    stemmed_reduced_embed_vocab = {}
-    for w in stemmed_reduced_vocab:
-        old_w_embed = [embed_vocab[w_old] for w_old in stemmed_dict[w]]
-        if embed_aggregate == 'mean':
-            new_w_embed = np.mean(old_w_embed, axis=0)
-        elif embed_aggregate == 'first':
-            new_w_embed = old_w_embed[0]
+    for i in emb:
+        if np.shape(emb[i])[0] > 1:
+            new_emb = np.mean(emb[i], axis=0)
+            emb[i] = np.array(new_emb)
         else:
-            print('Unknown embedding aggregation')
-            break
-        stemmed_reduced_embed_vocab[w] = new_w_embed
+            emb[i] = np.array(emb[i])
 
-    return (stemmed_reduced_vocab,
-            stemmed_reduced_embed_vocab,
-            stemmed_reduced_bow_data)
+    # 2. stop words
+    stop_words = set(stopwords.words('english'))
+
+    # 3. word count < 5
+    word_counts = vocab_BOW_new.sum(axis=0)
+
+    # 4. word length < 3
+    not_short = np.array([len(w) > 2 for w in vocab_new])
+
+    judg = np.zeros(np.size(vocab_new))
+    for i,w in enumerate(vocab_new):
+        judg[i] = (w not in stop_words)
+
+    judg = judg * not_short * (word_counts > 5)
+
+    vocab_new2 = list(np.array(vocab_new)[judg==1])
+    vocab_BOW_new2 = vocab_BOW_new[:,judg==1].astype(np.int)
+    emb2 = emb
+
+    w2 = np.array(vocab_new)[judg==0]
+    for w in w2:
+        del emb2[w]
+
+    return (vocab_new2,
+            emb2,
+            vocab_BOW_new2)
 
 
-def change_embeddings(vocab, bow_data, embed_path):
-    """Change embedding data if vocabulary has been reduced."""
+def embeddings_new(vocab, vocab_BOW, embed_path):
+
     all_embed_vocab = {}
     with open(embed_path, 'r') as file:
         for line in file.readlines():
@@ -71,27 +88,26 @@ def change_embeddings(vocab, bow_data, embed_path):
             embedding = [float(x) for x in line.split(' ')[1:]]
             all_embed_vocab[word] = embedding
 
-    data_embed_vocab = {}
+    emb3 = {}
     new_vocab_idx = []
-    new_vocab = []
+    vocab3 = []
     for i, w in enumerate(vocab):
         if w in all_embed_vocab:
-            data_embed_vocab[w] = all_embed_vocab[w]
+            emb3[w] = all_embed_vocab[w]
             new_vocab_idx.append(i)
-            new_vocab.append(w)
-    bow_data = bow_data[:, new_vocab_idx]
-    return new_vocab, data_embed_vocab, bow_data
-  
+            vocab3.append(w)
+    BOW3 = vocab_BOW[:, new_vocab_idx]
+    return vocab3, emb3, BOW3
+
+
 # loader
 def loader(data_path,
            embeddings_path,
            p=1,
-           K_lda=70,
+           K_lda=3,
            glove_embeddings=True,
            stemming=True):
-
-
-    data_all = sio.loadmat('amazon-emd_tr_te_split.mat', squeeze_me=True, chars_as_strings=True)  # dict
+    data_all = sio.loadmat(data_path, squeeze_me=True, chars_as_strings=True)  # dict
 
     if 'Y' in data_all:
         y_all = data_all['Y'].astype(np.int)
@@ -147,20 +163,16 @@ def loader(data_path,
 
         vocab_BOW[i, words_idx] = bow_i.astype(np.int)
 
-    ####################################################
-    # Use GLOVE word embeddings
+
     if glove_embeddings:
-        vocab, vocab_embed, vocab_BOW = change_embeddings(
+        vocab, vocab_embed, vocab_BOW = embeddings_new(
             vocab, vocab_BOW, embeddings_path)
-    # Reduce vocabulary by removing short words, stop words, and stemming
-    if stemming:
-        vocab, vocab_embed, vocab_BOW = reduce_vocab(
-            vocab_BOW, vocab, vocab_embed, embed_aggregate='mean')
 
+    if stemming:
+        vocab, vocab_embed, vocab_BOW = stem_vocab(
+            vocab_BOW, vocab, vocab_embed)
 
     ####################################################
-
-
 
     l1_BOW, l2_BOW = vocab_BOW.shape
     embed_dat = [[] for _ in range(l1_BOW)]
@@ -170,39 +182,53 @@ def loader(data_path,
                 for _ in range(vocab_BOW[d, i]):
                     embed_dat[d].append(vocab_embed[vocab[i]])
 
-    embed_data = []
+    vocab_embed = []
     for doc_i in embed_dat:
-        embed_data.append(np.array(doc_i))
-
+        vocab_embed.append(np.array(doc_i))
 
     # Matrix of word embeddings
     embeddings = np.array([vocab_embed[w] for w in vocab])
-
 
     model = lda.LDA(n_topics=K_lda, n_iter=1500, random_state=1)
     model.fit(vocab_BOW)
     topics = model.topic_word_
     lda_centers = np.matmul(topics, embeddings)
-    n_top_words = 20
-    for i, topic_dist in enumerate(topics):
-        topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n_top_words + 1):-1]
-
+    n_top_words = 10
+    topic_dict = {}
     topic_proportions = model.doc_topic_
 
-    cost_embeddings = euclidean_distances(embeddings, embeddings) ** p
+
+    #cost_embeddings_cos = cosine_similarity(embeddings, embeddings)
+    cost_embeddings = euclidean_distances(embeddings, embeddings)**p
     cost_topics = np.zeros((topics.shape[0], topics.shape[0]))
+    cost_m = np.zeros((topics.shape[0], topics.shape[0]))
 
     for i in range(cost_topics.shape[0]):
         for j in range(i + 1, cost_topics.shape[1]):
-            cost_topics[i, j] = ot.emd2(topics[i], topics[j], cost_embeddings, numItermax =10000)
+            #print(i,j)
+            # i_list = topic_dict[i].astype(bool)
+            # j_list = topic_dict[j].astype(bool)
+            #
+            # topic_i = topics[i][i_list]
+            # topic_j = topics[j][j_list]
+            #
+            # cost_e = cost_embeddings[i_list][:,j_list]
+            # # np.ascontiguousarray(topic_i)
+            # print(topic_i.flags['C_CONTIGUOUS'])
+            # # np.ascontiguousarray(topic_j)
+            # print(topic_j.flags['C_CONTIGUOUS'])
+            # cost_e = np.ascontiguousarray(cost_e)
+            # print(cost_e.flags['C_CONTIGUOUS'])
+            # cost_m[i,j] = ot.emd2(topic_i, topic_j, cost_e, numItermax=10000)
+            cost_topics[i, j] = ot.emd2(topics[i], topics[j], cost_embeddings,numItermax=10000)
     cost_topics = cost_topics + cost_topics.T
 
     output = {'X': vocab_BOW,
-           'y': y_all-1,
-           'embeddings': embeddings,
-           'topics': topics,
-           'proportions': topic_proportions,
-           'cost_E': cost_embeddings,
-           'cost_T': cost_topics}
+              'y': y_all - 1,
+              'embeddings': embeddings,
+              'topics': topics,
+              'proportions': topic_proportions,
+              'cost_E': cost_embeddings,
+             'cost_T': cost_topics}
 
     return output
