7b3794664cfe5352fea38ca6416a3798a5f922b5
jiesparklin <33667998+jiesparklin@users.noreply.github.com>
2019-12-10 16:59:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,53 +16,32 @@ data_path = './data/'
 # Download GloVe 6B tokens, 300d word embeddings from
 # https://nlp.stanford.edu/projects/glove/
 # and put them into
-embeddings_path = './data/glove.6B.50d.txt'
+embeddings_path = './data/glove.6B.300d.txt'
 
 # Pick a dataset (uncomment the line you want)
 data_name = 'bbcsport-emd_tr_te_split.mat'
-# data_name = 'twitter-emd_tr_te_split.mat'
-# data_name = 'r8-emd_tr_te3.mat'
 # data_name = 'amazon-emd_tr_te_split.mat'
-# data_name = 'classic-emd_tr_te_split.mat'
-# data_name = 'ohsumed-emd_tr_te_ix.mat'
 
-# Pick a seed
-# 0-4 for bbcsport, twitter, amazon, classic
-# r8 and ohsumed have a pre-defined train/test splits - just set seed=0
-seed = 0
-p = 1
 
-data = loader(data_path + data_name, embeddings_path, p=p)
+data = loader(data_path + data_name, embeddings_path, p=1)
 
 bow_data, y = data['X'], data['y']
 topic_proportions = data['proportions']
-
-bow_train, y_train, bow_test, y_test = train_test_split(bow_data, y)
+ ## specified in the paper using train_test_split to separate train and test sets.
+bow_train, class_train, bow_test, class_test = train_test_split(bow_data, y)
 topic_train, topic_test = train_test_split(topic_proportions)
 
-# Pick a method among RWMD, WMD, WMD-T20, HOTT, HOFTT
-methods = {'HOTT': hott.hott,
-           'HOFTT': hott.hoftt,
-           'WMD': distances.wmd,
-           'RWMD': distances.rwmd,
-           'WMDT': lambda p, q, C: distances.wmd(p, q, C, truncate=20)}
-method = 'HOTT'
+cost_bow = data['cost_E']
+cost_topic = data['cost_T']
 
-# p=1 for W1 and p=2 for W2
-cost_E = data['cost_E']
-cost_T = data['cost_T']
-
-# Get train/test data representation and transport cost
-if method in ['HOTT', 'HOFTT']:
-    # If method is HOTT or HOFTT train LDA and compute topic-topic transport cost
-    X_train, X_test = topic_train, topic_test
-    C = data['cost_T']
-else:
-    # Normalize BOW and compute word-word transport cost
-    X_train, X_test = normalize(bow_train, 'l1'), normalize(bow_test, 'l1')
-    C = data['cost_E']
+method = HOTT
+#method = HOFTT
+#method = WMD
+#method = WMDT20
 
 # Compute test error
-test_error = myknn(5,X_train,y_train, X_test, y_test, methods[method])
-
-# Done!
+if method == HOTT or method == HOFTT:
+    test_error = myknn(7,topic_train,class_train, topic_test, class_test, cost_topic, method)
+else:
+    test_error = myknn(7,normalize(bow_train, 'l1'),class_train,normalize(bow_test, 'l1'), class_test, cost_bow, method)
+print("test error: ", test_error)
